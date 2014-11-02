e56c260cf0e4ca6e2d88eed90a56595fc78dd2c5
ataalla <ataalla@pedram>
2014-11-02 11:08:40 +0330

    fixed tagedit with autocomplete and source

--- apps/api/controllers/UserController.php
+++ apps/api/controllers/UserController.php
@@ -9,13 +9,4 @@ class UserController extends ControllerBase {
     public function getAction($id) {
         return $this->getResponse(BaseUser::findFirst($id)->getPublicResponse());
     }
-
-    public function listAction() {
-        $results = array();
-        foreach (BaseUser::find() as $value) {
-            $results[] = $value->getPublicResponse();
-        }
-        return $this->getResponse($results);
-    }
-
 }
--- apps/frontend/controllers/IndexController.php
+++ apps/frontend/controllers/IndexController.php
@@ -2,9 +2,26 @@
 
 namespace Simpledom\Frontend\Controllers;
 
+use Simpledom\Core\AtaForm;
 use Simpledom\Frontend\BaseControllers\IndexControllerBase;
+use TagEditElement;
 
 class IndexController extends IndexControllerBase {
 
- 
+    public function testAction() {
+        $element = new TagEditElement("test");
+        $element->setDefault(implode(",", array("سلام", "شیراز")));
+//        $element->setAutocompleteSource("function(query ,response){"
+//                . "console.log(query ,response);"
+//                . "response(query.term + 'yes');"
+//                . "}");
+
+        $element->setAutocompleteSource("'http://melk.edspace.org/api/user/list'");
+
+        $fr = new AtaForm();
+        $fr->add($element);
+        $this->view->form = $fr;
+        $this->handleFormScripts($fr);
+    }
+
 }
