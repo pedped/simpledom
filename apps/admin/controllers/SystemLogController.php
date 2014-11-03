b8d7f98f5b183c94c1a2776f1d957b68d6434045
ataalla <ataalla@pedram>
2014-11-03 07:58:37 +0330

    systemlogcontroller renamed

--- apps/admin/controllers/SystemLogController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Simpledom\Admin\Controllers;
-
-use Simpledom\Admin\BaseControllers\SystemLogControllerBase;
-
-class SystemLogController extends SystemLogControllerBase {
-    
-}
