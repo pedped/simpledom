11b173e9386b20679d2cd4ea0c3789fa624f6e28
ataalla <ataalla@192.168.1.2>
2014-11-18 14:14:20 +0330

    Email Template Controller Fixed

---
+++ apps/admin/controllers/EmailtemplateController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Simpledom\Admin\Controllers;
+
+use Simpledom\Admin\BaseControllers\EmailTemplateControllerBase;
+
+class EmailTemplateController extends EmailTemplateControllerBase {
+    
+}
