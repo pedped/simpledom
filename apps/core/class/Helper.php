915534ec33d3b992e2626a26822570475a285727
edspace <edspace@edspace-PC>
2014-11-24 16:30:42 +0330

    wordLimiter function added view has been changed

--- apps/core/class/Helper.php
+++ apps/core/class/Helper.php
@@ -76,4 +76,18 @@ class Helper {
         }
     }
 
+    public static function wordLimiter($str, $limit = 100, $end_char = '&#8230;') {
+        if (trim($str) == '') {
+            return $str;
+        }
+
+        preg_match('/^\s*+(?:\S++\s*+){1,' . (int) $limit . '}/', $str, $matches);
+
+        if (strlen($str) == strlen($matches[0])) {
+            $end_char = '';
+        }
+
+        return rtrim($matches[0]) . $end_char;
+    }
+
 }
