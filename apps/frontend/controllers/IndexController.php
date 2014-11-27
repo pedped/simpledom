1a233917c9f9de7697c34df7ab959cbfecc963ba
ataalla <ataalla@192.168.1.2>
2014-11-27 11:30:15 +0330

    added slider element

--- apps/frontend/controllers/IndexController.php
+++ apps/frontend/controllers/IndexController.php
@@ -3,22 +3,22 @@
 namespace Simpledom\Frontend\Controllers;
 
 use PriceViewer;
+use RangeSlider;
 use Simpledom\Core\AtaForm;
 use Simpledom\Frontend\BaseControllers\IndexControllerBase;
 use SMSCreditCost;
-use TagEditElement;
 
 class IndexController extends IndexControllerBase {
 
     public function testAction() {
-        $element = new TagEditElement("test");
-        $element->setDefault(implode(",", array("سلام", "شیراز")));
-//        $element->setAutocompleteSource("function(query ,response){"
-//                . "console.log(query ,response);"
-//                . "response(query.term + 'yes');"
-//                . "}");
 
-        $element->setAutocompleteSource("'http://melk.edspace.org/api/user/list'");
+        $element = new RangeSlider("slider");
+        $element->min = 50;
+        $element->max = 500;
+        $element->currentMinValue = 65;
+        $element->currentMaxValue = 256;
+        $element->betweenRangeTitle = "تا";
+
 
         $fr = new AtaForm();
         $fr->add($element);
@@ -37,7 +37,8 @@ class IndexController extends IndexControllerBase {
             "کد",
             "تیتر",
         ));
-        
+
+
         $this->view->plansss = $viewr->Create();
     }
 
