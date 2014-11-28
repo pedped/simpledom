3f8ff1c3d0be0e4c836cfa4f71d2be267413a9c8
ataalla <ataalla@192.168.1.2>
2014-11-28 14:30:58 +0330

    fixed milion text fixed phone require

--- apps/frontend/forms/RequestMelkForm.php
+++ apps/frontend/forms/RequestMelkForm.php
@@ -18,12 +18,13 @@ class RequestMelkForm extends MelkSearch {
         $private_mobile->setInfo("کد تایید درخواست و مشخصات املاک به شماره شما ارسال خواهد گردید، پس در وارد کردن آن دقت نمایید");
         //$private_mobile->setAttribute('placeholder', 'Enter your Private Mobile');
         $private_mobile->setAttribute('class', 'form-control');
+        $private_mobile->setAttribute('required', 'required');
         $private_mobile->addValidator(new PresenceOf(array(
         )));
         $this->add($private_mobile);
 
 
-        
+
         // Submit Button
         $submit = new Submit('submit');
         $submit->setAttribute("value", _("Submit"));
