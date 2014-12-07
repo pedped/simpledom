658c4d315ae702bbf491c7602193b65b7525d154
edspace <edspace@edspace-PC>
2014-12-07 16:05:01 +0330

    product type renamed

---
+++ apps/admin/controllers/ProducttypeController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Simpledom\Admin\Controllers;
+
+use Simpledom\Admin\BaseControllers\ProductTypeControllerBase;
+
+class ProductTypeController extends ProductTypeControllerBase {
+    
+}
