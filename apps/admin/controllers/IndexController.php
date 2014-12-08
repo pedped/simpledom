65ffe9f7052b663bdccd1bf26a63dd819a895658
ataalla <ataalla@pedram>
2014-12-08 21:48:55 +0330

    added UserPhoneChart

--- apps/admin/controllers/IndexController.php
+++ apps/admin/controllers/IndexController.php
@@ -2,12 +2,16 @@
 
 namespace Simpledom\Admin\Controllers;
 
+use BaseUser;
 use Bongah;
 use BongahSentMelk;
 use BongahSubscriber;
+use LineChartElement;
 use Melk;
 use MelkPhoneListner;
+use ModelChart;
 use Simpledom\Admin\BaseControllers\IndexControllerBase;
+use Simpledom\Core\AtaForm;
 use Simpledom\Core\Classes\Helper;
 use UserOrder;
 use UserPhone;
@@ -16,6 +20,9 @@ class IndexController extends IndexControllerBase {
 
     public function indexAction() {
         parent::indexAction();
+
+        // load chart
+        $this->loadAmlakGostarCharts();
 
         // load total bongahs
         $this->view->totalBongahs = Bongah::count();
@@ -40,6 +47,29 @@ class IndexController extends IndexControllerBase {
 
         // Today User Phone
         $this->view->todayUserPhone = UserPhone::Count(array("date >= :date:", "bind" => array("date" => Helper::GetTodayStartTime())));
+    }
+
+    public function loadAmlakGostarCharts() {
+        // create new form
+        $form = new AtaForm();
+        $user = new BaseUser();
+
+        // load chart box
+        // fetch data
+        $modelChart = new ModelChart("userphonechart", new UserPhone());
+        $chartlement = $modelChart->getChart();
+        $chartlement->setTitle("Register Chart");
+        $chartlement->setSubtitle("total register count per day");
+        $chartlement->setXName("Date");
+        $chartlement->setYAxis("Count");
+        $chartlement->setValues($user->getLastMonthRegistarChart());
+
+        // add element to form
+        $form->add($chartlement);
+
+        // set view form
+        $this->view->form = $form;
+        $this->handleFormScripts($form);
     }
 
 }
