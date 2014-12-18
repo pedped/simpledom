70cd8c26059a8126fcf98d233b3eed3f85724b7f
Ata Alla Zangene Madar <convertersoft@gmail.com>
2014-12-18 15:07:24 +0330

    added notification check

--- apps/api/controllers/PublicController.php
+++ apps/api/controllers/PublicController.php
@@ -3,11 +3,32 @@
 namespace Simpledom\Api\Controllers;
 
 use BaseUser;
+use MobileNotification;
 use MobileToken;
 use stdClass;
 
 class PublicController extends ControllerBase {
 
+    public function newnotificationAction() {
+
+        // get last visit
+        $lastvisit = $this->request->getPost("lastvisit");
+
+        // check for last visit
+        if (!isset($lastvisit) || strlen($lastvisit) == 0) {
+            return $this->getResponse(false);
+        }
+
+        // get new notfications
+        $notification = MobileNotification::findFirst(array("releasedate >= :releasedate: AND enable = 1", "bind" => array("releasedate" => $lastvisit)));
+        if (!$notification) {
+            // there is no new notification
+            return $this->getResponse(false);
+        }
+
+        // send notification
+        return $this->getResponse($notification->getPublicResponse());
+    }
 
     /**
      * this function will get some information and register new account for the user
