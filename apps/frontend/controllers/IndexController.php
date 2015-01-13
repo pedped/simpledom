2c1a1e1d028c62ca94e43727f9301888d7b0532f
Ata Alla Zangene Madar <convertersoft@gmail.com>
2015-01-13 07:14:36 +0330

    added buy with mobile

--- apps/frontend/controllers/IndexController.php
+++ apps/frontend/controllers/IndexController.php
@@ -5,8 +5,10 @@ namespace Simpledom\Frontend\Controllers;
 use PriceViewer;
 use RangeSlider;
 use Simpledom\Core\AtaForm;
+use Simpledom\Core\Classes\Order;
 use Simpledom\Frontend\BaseControllers\IndexControllerBase;
 use SMSCreditCost;
+use UserOrder;
 
 class IndexController extends IndexControllerBase {
 
@@ -40,6 +42,27 @@ class IndexController extends IndexControllerBase {
 
 
         $this->view->plansss = $viewr->Create();
+    }
+    
+     public function buywithmobileAction($orderid) {
+
+
+        // check if user is not logged in, show 404
+        if (!isset($this->user)) {
+            $this->show404();
+            return;
+        }
+
+        // check if order id is valid
+        $order = UserOrder::findFirst(array("id = :id:", "bind" => array("id" => $orderid)));
+        if ($order == FALSE) {
+            $this->show404();
+            return;
+        }
+
+        // we have to request pay order
+        $orderObject = new Order($this->user->userid);
+        $orderObject->PayOrder($this->errors, $order->id, 1);
     }
 
 }
