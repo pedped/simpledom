093b5fae67e3213cd0bc0dd7f2356e9202ae7f57
Ata Alla Zangene Madar <convertersoft@gmail.com>
2015-01-17 10:38:50 +0330

    removed highchart  from indexaction

--- apps/core/frontendcontrollers/IndexControllerBase.php
+++ apps/core/frontendcontrollers/IndexControllerBase.php
@@ -2,17 +2,9 @@
 
 namespace Simpledom\Frontend\BaseControllers;
 
-use Simpledom\Core\MasterTutorialForm;
-
 class IndexControllerBase extends ControllerBase {
 
     public function indexAction() {
-
-        $this->AddUserLog("Visit Home Page");
-
-        $fr = new MasterTutorialForm();
-        $this->handleFormScripts($fr);
-        $this->view->form = $fr;
 
         $this->setPageTitle(_("Homepage"));
     }
